9458053492465b44a937063bd44dbd649b660153
Rakeshkawade18 <112071576+Rakeshkawade18@users.noreply.github.com>
2022-08-25 16:57:21 +0530

    Update streamlit.app.py
    fixed typo

--- streamlit.app.py
+++ streamlit.app.py
@@ -4,4 +4,4 @@ import streamlit
 streamlit('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & blueberry oatmel')
-streamit.text('Hard boiled Free Range egg')
+streamlit.text('Hard boiled Free Range egg')
