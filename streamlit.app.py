0aac77200aee53fd79ef9ce24bef0da044327ec4
Rakeshkawade18 <112071576+Rakeshkawade18@users.noreply.github.com>
2022-08-29 16:27:09 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -5,3 +5,7 @@ streamlit('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & blueberry oatmel')
 streamlit.text('Hard boiled Free Range egg')
+
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
